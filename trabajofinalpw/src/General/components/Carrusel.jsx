a8f41b20f3d7eab98ac3eafb46a72bf6bf49bc16
RodrigoTF <rodrigon_5@hotmail.com>
2022-07-18 11:40:39 -0500

    Carpeta Paciente

--- trabajofinalpw/src/General/components/Carrusel.jsx
+++ trabajofinalpw/src/General/components/Carrusel.jsx
@@ -1,3 +1,4 @@
+
 const Carrusel = () => {
     return <div key="carouselExampleDark" className="carousel carousel-dark slide" data-bs-ride="carousel">
         <div className="carousel-indicators">
