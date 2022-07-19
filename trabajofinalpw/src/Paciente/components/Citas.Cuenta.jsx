f499db10f71274c2fba065e74d8ce8301938c6e8
RodrigoTF <rodrigon_5@hotmail.com>
2022-07-19 06:50:41 -0500

    images

--- trabajofinalpw/src/Paciente/components/Citas.Cuenta.jsx
+++ trabajofinalpw/src/Paciente/components/Citas.Cuenta.jsx
@@ -2,7 +2,7 @@ const Cuenta = () => {
     return <div className="card mb-3">
         <div className="row g-0">
             <div className="col-md-4">
-                <img src="/pw-trabajogrupal/paciente/imagenes/default-avatar.png" className="img-fluid ms-5 my-3 d-block p-4 border-end border-2" alt="..." width="200" />
+                <img src="" className="img-fluid ms-5 my-3 d-block p-4 border-end border-2" alt="..." width="200" />
             </div>
             <div className="col-md-8 align-self-center">
                 <div className="card-body">
